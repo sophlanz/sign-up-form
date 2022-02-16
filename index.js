d47245fee9802be4b454916150d8092f508a5ec3
Sophlanz <sophialanzendorf@gmail.com>
2022-02-16 22:23:55 +0100

    completed styling of left side of page

---
+++ index.js
@@ -0,0 +1,19 @@
+ document.getElementById('passwordConfirm').addEventListener('change', passwordMatch);
+ function passwordMatch () {
+    const password = document.getElementById("password");
+    const passwordConfirm = document.getElementById("passwordConfirm");
+    const errorMsg = document.getElementById("errorMsg");
+    if (password.value !== passwordConfirm.value) {
+        errorMsg.innerHTML = "* Passwords do not match";
+        password.setAttribute ("style", "border-color:red");
+        passwordConfirm.setAttribute ("style", "border-color:red");
+
+    } else {
+        errorMsg.innerHTML= "";
+         password.setAttribute ("style", "border-color:green");
+        passwordConfirm.setAttribute ("style", "border-color:green");
+        
+    }
+   
+    
+}
